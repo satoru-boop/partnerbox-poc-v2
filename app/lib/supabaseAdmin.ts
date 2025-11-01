6ead2ad05bff4627b59a0c8d53c69254c4b77ae1
satoru-boop <satoru@hedell-group.ltd>
2025-11-01 15:22:43 +0900

    Create supabaseAdmin.ts

---
+++ app/lib/supabaseAdmin.ts
@@ -0,0 +1,8 @@
+// app/lib/supabaseAdmin.ts
+import { createClient } from '@supabase/supabase-js'
+
+export const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!, // ← Vercelに入れたservice_role
+  { auth: { persistSession: false } }
+)
