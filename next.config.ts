7b2ceb30d1ce3fad09e9598aa63eb13178617928
satoru-boop <satoru@hedell-group.ltd>
2025-11-01 17:10:28 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,9 @@ import path from 'path';
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    // 念のため存在ガード
-    // @ts-expect-error — Next.jsの型が狭い場合に備えて
     config.resolve = config.resolve || {};
-    // @ts-expect-error
     config.resolve.alias = {
-      // @ts-expect-error
-      ...(config.resolve.alias || {}),
+    ...(config.resolve.alias || {}),
       '@': path.resolve(__dirname), // ルート直下を '@' に割当て
     };
     return config;
